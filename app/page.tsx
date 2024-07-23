982d9438b5e571af4944c3302040417f6651c61d
issamelimrany <issamelimrany@hotmail.com>
2024-07-24 00:01:46 +0100

    changed the basic form management component and added a CalculTva functionality for testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
-import { Input } from "../components/input";
+"use client";  // Add this line at the top
+
+import CalculTVA from "../components/CalculTva";
 
 export default function Home() {
   return (
     <div>
-      hello type here : 
-      <Input />
+      <br></br>
+      <br></br>
+      <br></br>
+      <CalculTVA />
     </div>
   );
 }
