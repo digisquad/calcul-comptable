c28d460e7561339f57fa87fbfbd051a261de3735
ISSAM EL IMRANY <90471805+issamelimrany@users.noreply.github.com>
2024-07-24 00:44:37 +0100

    Setup1 (#2)
    * a minimalist configuration of next.js, basic storybook configuration and a input component ( that'll be utilized in the form )
    
    * added some basic form management components
    
    * cleaned some files
    
    * changed the basic form management component and added a CalculTva functionality for testing
    
    * added storybook stories for each component

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+"use client";  // Add this line at the top
+
+import CalculTVA from "../components/CalculTva";
+
+export default function Home() {
+  return (
+    <div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <CalculTVA />
+    </div>
+  );
+}
