5ad7222d48c27b7d4a1a300f6f9e3872a3b8e050
ISSAM EL IMRANY <90471805+issamelimrany@users.noreply.github.com>
2024-08-06 21:10:28 +0100

    idk

--- components/ui/Form/InputWithLabel/InputWithLabel.tsx
+++ components/ui/Form/InputWithLabel/InputWithLabel.tsx
@@ -1,16 +1,14 @@
-import * as React from "react";
-import * as LabelPrimitive from "@radix-ui/react-label";
-import { cva, type VariantProps } from "class-variance-authority";
-import { cn } from "@/lib/utils";
+import * as React from "react"
+import * as LabelPrimitive from "@radix-ui/react-label"
+import { cva, type VariantProps } from "class-variance-authority"
+import { cn } from "@/lib/utils"
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  label: string;
-  labelClassName?: string;
+  label: string
+  labelClassName?: string
 }
 
-const labelVariants = cva(
-  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-);
+const labelVariants = cva("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70")
 
 const InputWithLabel = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, label, labelClassName, id, ...props }, ref) => {
@@ -27,16 +25,16 @@ const InputWithLabel = React.forwardRef<HTMLInputElement, InputProps>(
           type={type}
           className={cn(
             "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-            className
+            className,
           )}
           ref={ref}
           {...props}
         />
       </div>
-    );
-  }
-);
+    )
+  },
+)
 
-InputWithLabel.displayName = "InputWithLabel";
+InputWithLabel.displayName = "InputWithLabel"
 
-export { InputWithLabel };
+export { InputWithLabel }
